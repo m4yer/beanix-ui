ec8aea0cc4425ccb0446ba3f5133e8f4e4d6fe82
Maksim Bogdanov <aspid888@gmail.com>
2022-07-16 11:31:36 +0300

    react-router:
    - BrowserRouter for connecting to browser url
    - <Routes>
    - <Route> for each "route"
    - <Link> for
    - Nested routes (<Route> within a <Route> to display repeating layouts)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import {Link, Outlet} from "react-router-dom";
+
 export default function App() {
     return (
         <>
             <h1>Just hi</h1>
+            <nav>
+                <Link to="/invoices">Invoices</Link>
+                <Link to="/expenses">Expenses</Link>
+            </nav>
+            <Outlet/>
         </>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {BrowserRouter, Route, Routes,} from "react-router-dom";
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App/>);
+root.render(
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App/>}>
+                <Route path="expenses" element={<Expenses/>}/>
+                <Route path="invoices" element={<Invoices/>}/>
+            </Route>
+        </Routes>
+    </BrowserRouter>
+);
 
